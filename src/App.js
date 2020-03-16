cddf1922e6f19d69d98168032dd83bd269c58d64
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-03-16 13:46:27 -0700

    added a 60 seconds timer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 //TODO: STEP 1 - Import the useState hook.
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 import Buttons from "./components/Buttons";
@@ -8,6 +8,19 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [hScore, setHScore] = useState(0);
   const [fScore, setFScore] = useState(0);
+  const [time, setTime] = useState(5);
+  const [id, setId] = useState(0);
+  const countRef = useRef(time);
+  countRef.current = time;
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTime(time - 1);
+    }, 1000);
+    setId(timer);
+  }, [time]);
+
+  console.log(time);
 
   const handlehScore = () => setHScore(hScore + 7);
   const handlefScore = () => setFScore(fScore + 3);
@@ -15,6 +28,9 @@ function App() {
   const awayhScore = () => setHScore(hScore - 7);
   const awayfScore = () => setFScore(fScore - 3);
 
+  if (time === 0) {
+    clearTimeout(id);
+  }
   return (
     <div className="container">
       <section className="scoreboard">
@@ -26,7 +42,7 @@ function App() {
 
             <div className="home__score">{hScore}</div>
           </div>
-          <div className="timer">00:03</div>
+          <div className="timer">{time}</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
             <div className="away__score">{fScore}</div>
